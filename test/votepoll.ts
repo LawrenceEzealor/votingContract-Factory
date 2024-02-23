8d2433568a4658928e37caa847e69860e1598a2e
Lawrence Ezealor <ezealor20@gmail.com>
2024-02-23 15:58:33 +0100

    unit testing

--- test/votepoll.ts
+++ test/votepoll.ts
@@ -13,29 +13,47 @@ describe("VotePoll", function () {
   
 
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    const [ owner, otherAccount] = await ethers.getSigners();
 
-    const VotePoll = await ethers.getContractFactory("VotePoll");
-    const votepoll = await VotePoll.deploy();
+    const question = "Who do you chose";
+    const options = ["Law", "ren"];
+    // const VotePoll = await ethers.getContractFactory("VotePoll");
+    // const votepoll = await VotePoll.deploy(question, options);
+    
+    const PollFactory = await ethers.getContractFactory("PollFactory");
+    const  pollFactory = await PollFactory.deploy();
+  
 
-    const hasNotVoted = await ethers.getContractFactory("VotePOll");
-    const notZeroAddress = "0x0000000000000000000000000000000000000000"
-
-    return { votepoll, notZeroAddress, hasNotVoted , owner, otherAccount };
+    return { owner, otherAccount, pollFactory };
   }
 
-  describe("Deployment", function () {
-    it("Should check that it is not address zero", async function () {
-      const { votepoll, notZeroAddress} = await loadFixture(deployVotePoll);
+  describe("Vote", function () {
+    it("Should create voting poll and emit event", async function () {
+      const { pollFactory } = await loadFixture(deployVotePoll);
 
-      expect(await votepoll.hasVoted(notZeroAddress)).to.equal(0);
+    const question = "Who do you chose";
+    const options = ["Law", "ren"];
+
+      const vote = await pollFactory.createPoll(question, options);
+      console.log(vote);
+      
+      // We are expecting a new created votepoll address
+      // How do we test for address
+
+      expect(vote).to.equal(vote);
     });
 
-  //   it("Should set the right owner", async function () {
-  //     const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+    //     it("Should reject address zero", async function () {
+    //   // const addressZero = "0x0000000000000000000000000000000000000000";
+    //   // const { votepoll, addressZero } = await loadFixture(deployVotePoll);
 
-  //     expect(await lock.owner()).to.equal(owner.address);
-  //   });
+    //   // expect(await votepoll.vote(addressZero)).to.equal(0);
+    // });
+    // it("Should set the right owner", async function () {
+    //   const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+
+    //   expect(await lock.owner()).to.equal(owner.address);
+    // });
 
   //   it("Should receive and store the funds to lock", async function () {
   //     const { lock, lockedAmount } = await loadFixture(
@@ -55,76 +73,8 @@ describe("VotePoll", function () {
   //       "Unlock time should be in the future"
   //     );
   //   });
-  // });
+  // });//STOP HERE
 
-
-
-
-
-
-  // describe("Withdrawals", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert with the right error if called too soon", async function () {
-  //       const { lock } = await loadFixture(deployOneYearLockFixture);
-
-  //       await expect(lock.withdraw()).to.be.revertedWith(
-  //         "You can't withdraw yet"
-  //       );
-  //     });
-
-  //     it("Should revert with the right error if called from another account", async function () {
-  //       const { lock, unlockTime, otherAccount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // We can increase the time in Hardhat Network
-  //       await time.increaseTo(unlockTime);
-
-  //       // We use lock.connect() to send a transaction from another account
-  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-  //         "You aren't the owner"
-  //       );
-  //     });
-
-  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-  //       const { lock, unlockTime } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       // Transactions are sent using the first signer by default
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).not.to.be.reverted;
-  //     });
-  //   });
-
-  //   describe("Events", function () {
-  //     it("Should emit an event on withdrawals", async function () {
-  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw())
-  //         .to.emit(lock, "Withdrawal")
-  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-  //     });
-  //   });
-
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
-  // });
+  
+  });
 });
